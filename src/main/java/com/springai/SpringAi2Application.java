a0b08268943ddadbeddf454ff78823236f037433
Vinod Kumar <vinodkumar@192.168.1.8>
2025-09-26 19:57:05 +0530

    Initial Commit

---
+++ src/main/java/com/springai/SpringAi2Application.java
@@ -0,0 +1,12 @@
+package com.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi2Application.class, args);
+    }
+}
---
+++ src/test/java/com/springai/SpringAi2ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi2ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
