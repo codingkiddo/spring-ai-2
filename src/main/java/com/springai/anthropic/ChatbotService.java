9116e420571dd831f5373f4a1f95c3770a0077f0
Vinod Kumar <vinodkumar@192.168.1.10>
2025-09-28 11:46:02 +0530

    new file:   src/main/java/com/springai/anthropic/ChatbotService.java

---
+++ src/main/java/com/springai/anthropic/ChatbotService.java
@@ -0,0 +1,25 @@
+package com.springai.anthropic;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatbotService {
+
+	private final ChatClient chatClient;
+
+    public ChatbotService(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public ChatResponse chat(ChatRequest chatRequest) {
+    	UUID chatId = Optional.ofNullable(chatRequest.chatId()).orElse(UUID.randomUUID());
+    	String answer = chatClient.prompt().user(chatRequest.question()).advisors(advisorSpec -> {
+    		advisorSpec.param("chat_memory_conversation_id", chatId);
+    	}).call().content();
+    	return new ChatResponse(chatId, answer);
+    }
+}
